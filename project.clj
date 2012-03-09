d98fa18ffc6310e9312533afaf9aeef2923bf22f
Myles Megyesi <mylesmegyesi@me.com>
2012-03-09 15:52:00 -0600

    updates version to 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metis "0.1.6"
+(defproject metis "0.1.7"
   :description "A library for validating maps."
   :url "https://github.com/mylesmegyesi/metis"
   :license {:name "Eclipse Public License - v 1.0"
