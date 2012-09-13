94f39f40276f8fd06754dc080529317011239487
Myles Megyesi <mylesmegyesi@me.com>
2012-09-13 16:41:28 -0500

    upgrade to leiningen 2

--- project.clj
+++ project.clj
@@ -5,6 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
-  :dev-dependencies [[speclj "2.1.1"]]
-  :test-path "spec/")
+
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+
+  ; leiningen 1
+  :dev-dependencies [[speclj "2.3.0"]]
+  :test-path "spec"
+
+  ; leiningen 2
+  :profiles {:dev {:dependencies [[speclj "2.3.0"]]}}
+  :test-paths ["spec/"]
+  :plugins [[speclj "2.3.0"]])
